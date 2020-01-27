b47319bea2e278b7fd91cd6609dbe59106e1fbe5
Marlrus <36910211+Marlrus@users.noreply.github.com>
2020-01-27 13:33:18 -0500

    Initial Commit: DB and Server Setup

---
+++ app.js
@@ -0,0 +1,31 @@
+const   express         = require('express'),
+        app             = express(),
+        mongoose        = require('mongoose'),
+        dotenv          = require('dotenv')
+
+//DOTENV
+dotenv.config()
+
+//REQUIRED ROUTES
+
+//VIEW ENGINE
+app.set('view engine', 'ejs')
+
+//LOCALS
+
+//DB CONNECTION
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(()=>{
+    console.log(`Mongoose Connected to: ${mongoose.connection.name}`)
+}).catch(err=>{
+    console.log(`Error: ${err.message}`)
+})
+
+//SERVER
+
+const port = process.env.PORT || 3000
+
+app.listen(port, ()=> console.log(`Email Verification Test server started in: ${port}`))
