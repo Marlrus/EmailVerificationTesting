4c1aecb42bf28a571eb4a69c09f9a1f407802bbf
Marlrus <36910211+Marlrus@users.noreply.github.com>
2020-01-28 13:21:06 -0500

    Created sendgrid sent email using nodemailer successfuly

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,13 @@ const   express     = require('express'),
         router      = express.Router({mergeParams:true}),
         passport    = require('passport'),
         User        = require('../models/user'),
-        middleware  = require('../middleware')
+        Token        = require('../models/token'),
+        middleware  = require('../middleware'),
+        crypto      = require('crypto'),
+        nodemailer  = require('nodemailer')
+
+//DOT ENV
+require('dotenv').config()
 
 //ROUTES
 router.get('/',(req,res)=>{
@@ -23,13 +29,42 @@ router.post('/sign-up',(req,res)=>{
         username: req.body.email
     })
     console.log(newUser)
-    User.register(newUser, req.body.password, (err,user)=>{
+    User.register(newUser, req.body.password, async(err,user)=>{
         console.log(`Created new User: ${user}`)
         if (err){
             return res.send(`Error: ${err}`)
         }
+        //Create & save verification token for this user
+        let token = await Token.create({
+            _userID: user._id,
+            token: crypto.randomBytes(16).toString('hex')
+        })
+        console.log('CREATED TOKEN:')
+        console.log(token)
+        //Send the Email CREATED SENDGRID
+        let transporter = nodemailer.createTransport({
+            service: 'Sendgrid',
+            auth: {
+                user: process.env.SENDGRID_USERNAME,
+                pass: process.env.SENDGRID_PASSWORD
+            }
+        })
+        let mailOptions = { 
+            from: 'no-reply@emailVerificationTest.com', 
+            to: user.email,
+            subject: 'Account Verification Token',
+            text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + token.token + '.\n'
+        }
+        transporter.sendMail(mailOptions, (err)=>{
+            console.log('INSIDE SENDMAIL')
+            res.send('A verification email was sent to: ' + user.email)
+        })
         passport.authenticate('local')(req,res,()=>{
-            res.redirect('/profile')
+            if(user.isVerified){
+                res.redirect('/profile')
+            }else{
+                res.send('Please verify your email')
+            }
         })
     })
 })
