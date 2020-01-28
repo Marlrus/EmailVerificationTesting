becfa9a757c2fef642b452f87c46caf1c3e2ca91
Marlrus <36910211+Marlrus@users.noreply.github.com>
2020-01-28 15:39:22 -0500

    Added resend of token, login not working

--- routes/index.js
+++ routes/index.js
@@ -53,36 +53,114 @@ router.post('/sign-up',(req,res)=>{
             from: 'no-reply@emailVerificationTest.com', 
             to: user.email,
             subject: 'Account Verification Token',
-            text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + token.token + '\n'
+            text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/verification\/' + token.token + '\n'
         }
         transporter.sendMail(mailOptions, (err)=>{
-            console.log('INSIDE SENDMAIL')
+            console.log('SENDING FIRST TOKEN')
         })
         passport.authenticate('local')(req,res,()=>{
             if(user.isVerified){
                 res.redirect('/profile')
             }else{
-                res.send(`A verification email was sent to ${user.email}, please verify to continue.`)
+                res.redirect('/verification')
             }
         })
     })
 })
 
-//CONFIRMATION LINK
-router.get('/confirmation/:token',async (req,res)=>{
+//verification Message
+router.get('/verification',(req,res)=>{
+    res.render('verification')
+})
+
+//verification RESEND
+router.post('/verification',async(req,res)=>{
+    //find user in DB
+    const foundUser = await User.findById(req.user._id)
+    console.log(foundUser)
+    console.log('Checking Emails')
+    //Handle if email is the same
+    if(foundUser.email === req.body.email){
+        console.log('Same Email')
+        //CAN BE MADE INTO MIDDLEWARE
+        //Create & save verification token for this user
+        let token = await Token.create({
+            _userID: foundUser._id,
+            token: crypto.randomBytes(16).toString('hex')
+        })
+        console.log('CREATED TOKEN:')
+        console.log(token)
+        //Send the Email CREATED SENDGRID
+        let transporter = nodemailer.createTransport({
+            service: 'Sendgrid',
+            auth: {
+                user: process.env.SENDGRID_USERNAME,
+                pass: process.env.SENDGRID_PASSWORD
+            }
+        })
+        let mailOptions = { 
+            from: 'no-reply@emailVerificationTest.com', 
+            to: foundUser.email,
+            subject: 'Account Verification Token',
+            text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/verification\/' + token.token + '\n'
+        }
+        transporter.sendMail(mailOptions, (err)=>{
+            console.log('SENDING NEW TOKEN')
+        })
+        //Return to verification Message
+        res.render('verification')
+    }else{
+        //change email and save
+        foundUser.email = req.body.email
+        foundUser.username = req.body.email
+        await foundUser.save()
+        console.log('Different Email Handler: UPDATING')
+        console.log(foundUser)
+        //Handle if email is different
+        let token = await Token.create({
+            _userID: foundUser._id,
+            token: crypto.randomBytes(16).toString('hex')
+        })
+        console.log('CREATED TOKEN:')
+        console.log(token)
+        //Send the Email CREATED SENDGRID
+        let transporter = nodemailer.createTransport({
+            service: 'Sendgrid',
+            auth: {
+                user: process.env.SENDGRID_USERNAME,
+                pass: process.env.SENDGRID_PASSWORD
+            }
+        })
+        let mailOptions = { 
+            from: 'no-reply@emailVerificationTest.com', 
+            to: foundUser.email,
+            subject: 'Account Verification Token',
+            text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/verification\/' + token.token + '\n'
+        }
+        transporter.sendMail(mailOptions, (err)=>{
+            console.log('SENDING NEW TOKEN')
+        })
+        //Return to verification Message
+        res.render('verification')
+    }
+})
+
+//verification LINK
+router.get('/verification/:token',async (req,res)=>{
+    console.log('VERIFICATION ROUTE')
     console.log(req.params.token)
     const foundToken = await Token.findOne({token: req.params.token})
-    // console.log(foundToken)
+    console.log(foundToken)
     if(!foundToken){
         console.log('Token not found')
         res.redirect('/')
     } else{
-        const confirmedUser = await User.findById(req.user._id)
-        // console.log(confirmedUser)
+        const confirmedUser = await User.findById(foundToken._userID)
+        console.log(confirmedUser)
         confirmedUser.isVerified = true
         await confirmedUser.save()
         // console.log(confirmedUser.isVerified)
-        // console.log(req.user)
+        console.log(req.user)
         res.redirect('/profile')  
     }
 })
@@ -103,7 +181,9 @@ router.get('/login', (req,res)=>{
 
 //Login Post for Verification
 router.post('/login', passport.authenticate('local'), async (req,res)=>{
-    const foundUser = await User.findById(req.user._id)
+    console.log('Inside the Login route')
+    const foundUser = await User.findOne({email: req.body.email})
+    console.log(foundUser)
     if(!foundUser.isVerified){
         res.send('User Email is not Verified')
     }else{
